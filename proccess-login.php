b0f117cc387825e2a27489c07bd7b09f73c89e5a
eskitzis <eskitzis@csd.auth.gr>
2023-01-21 17:50:49 +0200

    commit

---
+++ proccess-login.php
@@ -0,0 +1,30 @@
+<?php
+    //values passed from index form
+    $username = $_POST['user'];
+    $password = $_POST['pass'];
+
+    //prevent injections
+    $username = stripcslashes($username);
+    $password = stripcslashes($password);
+
+    $con = mysqli_connect('', '', '', '');
+
+    //Query the database for user
+    $result = mysqli_query($con, "select * from '' where username = '$username' and password = '$password'") 
+              or die("Failed to query database".mysql_error());
+    $row = mysqli_fetch_array($result, MYSQLI_BOTH);
+
+    if ($row['username'] == $username && $row['password'] == $password && $row['role'] == "employee"){
+        //echo "Login Success!!! Welcome ".$row['username']." ".$row['role'];
+        session_start();
+        header("Location: employee.php");
+        $_SESSION['fname'] = $row['fullname'];
+    }
+
+    if ($row['username'] == $username && $row['password'] == $password && $row['role'] == "admin"){
+        //echo "Login Success!!! Welcome ".$row['username']." ".$row['role'];
+        session_start();
+        header("Location: admin.php");
+        $_SESSION['fname'] = $row['fullname'];
+    }
+?>
