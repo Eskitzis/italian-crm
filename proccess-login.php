d5640d34985a349afc31292686c4ffd53b682fc8
eskitzis <eskitzis@csd.auth.gr>
2023-02-14 02:03:10 +0200

    dd

--- proccess-login.php
+++ proccess-login.php
@@ -7,7 +7,7 @@
     $username = stripcslashes($username);
     $password = stripcslashes($password);
 
-    $con = mysqli_connect('server.onsight.gr', 'gruppocasa', 'b2tV*5e3', 'italiancrm');
+    $con = mysqli_connect('server.onsight.gr:8443', 'gruppocasa', 'b2tV*5e3', 'italiancrm');
 
     //Query the database for user
     $result = mysqli_query($con, "select * from 'users' where username = '$username' and password = '$password'") 
@@ -17,14 +17,14 @@
     if ($row['username'] == $username && $row['password'] == $password && $row['role'] == "employee"){
         //echo "Login Success!!! Welcome ".$row['username']." ".$row['role'];
         session_start();
-        header("Location: home.php");
+        header("Location: home.html");
         $_SESSION['fname'] = $row['fullname'];
     }
 
     if ($row['username'] == $username && $row['password'] == $password && $row['role'] == "admin"){
         //echo "Login Success!!! Welcome ".$row['username']." ".$row['role'];
         session_start();
-        header("Location: home.php");
+        header("Location: home.html");
         $_SESSION['fname'] = $row['fullname'];
     }
 ?>
