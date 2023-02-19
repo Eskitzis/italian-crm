6a39a14e4ae984f0bbf0a479a6bd34b6ac981d82
eskitzis <eskitzis@csd.auth.gr>
2023-02-19 14:48:02 +0200

    ss

--- customers.php
+++ customers.php
@@ -421,6 +421,7 @@ if ($conn->connect_error) {
                     echo '<div class="product-cell price"><span class="cell-label">Factorys:</span>' . $row['factorys'] .'</div>';
                     echo '<div class="product-cell price"><span class="cell-label">Status Last Order:</span><span class="status active">' . $row['lastupdate'] .'</span></div>';
                     echo '<div class="product-cell price"><span class="cell-label">Archive:</span>' . $row['archive'] .'</div>';                      
+                    echo '<br>';
                     echo $customercartela; 
                     echo '</div>';
                   }
