618ab00fe5383a52eba52c3de5ecdf7cc1695afa
eskitzis <eskitzis@csd.auth.gr>
2023-02-19 14:31:39 +0200

    aaa

--- customers.php
+++ customers.php
@@ -410,6 +410,7 @@ if ($conn->connect_error) {
                 // Output data of each row
                   while($row = $result->fetch_assoc()) {
                     // Create divs based on the data
+                    echo $customercartela;
                     echo '<div class="products-row" id="customer1" onclick="cartela(this.id)">';
                     echo '<button class="cell-more-button"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg></button>';
                     echo '<div class="product-cell image"><!--IMAGE GOES HERE--><span>' . $row['firstname'] .' '. $row['lastname'] . '</span></div>';
@@ -423,7 +424,6 @@ if ($conn->connect_error) {
                     echo '<div class="product-cell price"><span class="cell-label">Archive:</span>' . $row['archive'] .'</div>';                      
                     echo '</div>';
                   }
-                  echo $customercartela;
                 }
             ?>
           </div>
