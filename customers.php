1286e0cea6c4971ea6f6e79db6e62b732f5bf5f2
eskitzis <eskitzis@csd.auth.gr>
2023-02-20 10:59:20 +0200

    1059

--- customers.php
+++ customers.php
@@ -467,16 +467,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             document.querySelector(".customer-menu").classList.toggle("active");
         });
         document.querySelector(".grid").addEventListener("click", function () {
-            document.querySelector(".grid").classList.remove("active");
-            document.querySelector(".list").classList.add("active");
-            document.querySelector(".products-area-wrapper").classList.add("tableView");
-            document.querySelector(".products-area-wrapper").classList.remove("gridView");
+            document.querySelector(".list").classList.remove("active");
+            document.querySelector(".grid").classList.add("active");
+            document.querySelector(".products-area-wrapper").classList.add("gridView");
+            document.querySelector(".products-area-wrapper").classList.remove("tableView");
         });
         document.querySelector(".list").addEventListener("click", function () {
-            document.querySelector(".grid").classList.add("active");
-            document.querySelector(".list").classList.remove("active");
-            document.querySelector(".products-area-wrapper").classList.remove("tableView");
-            document.querySelector(".products-area-wrapper").classList.add("gridView");
+            document.querySelector(".list").classList.add("active");
+            document.querySelector(".grid").classList.remove("active");
+            document.querySelector(".products-area-wrapper").classList.remove("gridView");
+            document.querySelector(".products-area-wrapper").classList.add("tableView");
         });
         var modeSwitch = document.querySelector('.mode-switch');
         modeSwitch.addEventListener('click', function () {                      
