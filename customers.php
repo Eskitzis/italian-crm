65b8c8936eabfaf221f85618f6315b74f6f48043
eskitzis <eskitzis@csd.auth.gr>
2023-02-20 11:31:13 +0200

    1131

--- customers.php
+++ customers.php
@@ -480,10 +480,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             document.querySelector(".products-area-wrapper").classList.remove("tableView");
         });
         document.querySelector(".list").addEventListener("click", function () {
-            document.querySelector(".list").classList.add("active");
-            document.querySelector(".grid").classList.remove("active");
-            document.querySelector(".products-area-wrapper").classList.remove("gridView");
-            document.querySelector(".products-area-wrapper").classList.add("tableView");
+            document.querySelector(".list").classList.remove("active");
+            document.querySelector(".grid").classList.add("active");
+            document.querySelector(".products-area-wrapper").classList.add("gridView");
+            document.querySelector(".products-area-wrapper").classList.remove("tableView");
         });
         var modeSwitch = document.querySelector('.mode-switch');
         modeSwitch.addEventListener('click', function () {                      
