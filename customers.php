6496831595f04d2a52381f3d3bb38169fffa981a
eskitzis <eskitzis@csd.auth.gr>
2023-02-22 16:20:05 +0200

    aa

--- customers.php
+++ customers.php
@@ -447,7 +447,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                   }
                 }
             ?>
-            <ul id="more-options-list">
+            <ul class="more-options-list">
               <li><a href="#">Option 1</a></li>
               <li><a href="#">Option 2</a></li>
               <li><a href="#">Option 3</a></li>
