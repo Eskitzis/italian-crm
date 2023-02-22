8e638ffedb2334e1a35bbaa19180954bd7a6ce28
eskitzis <eskitzis@csd.auth.gr>
2023-02-22 19:08:47 +0200

    708

--- customers.php
+++ customers.php
@@ -477,7 +477,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     		        <input class="modal-input" type="text" name="" id="">
   		        </div>
               <br>
-              <div style="-ms-transform: translateY(-50%); transform: translateY(-50%);">
+              <div class="buttons-center">
                 <button class="modal-button-blue" type="button" onclick="customReset()">Reset</button>
                 <button class="modal-button-green" type="submit" value="submit">Apply</button>
               </div>
