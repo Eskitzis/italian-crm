92ae9d8bb45ef02ea65f0259efd60eaaf1bdade8
eskitzis <eskitzis@csd.auth.gr>
2023-02-22 16:33:35 +0200

    433

--- customers.php
+++ customers.php
@@ -428,43 +428,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <!--END DATA ARCHIVE-->  
                     </div>
                 ';
-                $customermenuphp = '
-                  <div class="customermenu">  
-                    <div class="customer-menu-buttons">
-                      <form action="" method="post">
-                        <input class="customer-input" type="text" name="firstname" id="firstname" placeholder="First Name" required>
-                        <br>
-                        <input class="customer-input" type="text" name="lastname" id="lastname" placeholder="Last Name" required>
-                        <br>
-                        <input class="customer-input" type="text" name="company" id="company" placeholder="Company">
-                        <br>
-                        <input class="customer-input" type="text" name="address" id="address" placeholder="Address">
-                        <br>
-                        <input class="customer-input" type="text" name="zip" id="zip" placeholder="ZIP/Postal Code">
-                        <br>
-                        <input class="customer-input" type="text" name="city" id="city" placeholder="City">
-                        <br>
-                        <input class="customer-input" type="text" name="country" id="country" placeholder="Country">
-                        <br>
-                        <input class="customer-input" type="email" name="email" id="email" placeholder="E-Mail">
-                        <br>
-                        <input class="customer-input" type="text" name="telephone" id="telephone" placeholder="Telephone">
-                        <br>
-                        <input class="customer-input" type="text" name="representive" id="representive" placeholder="Representative/Salesman">
-                        <br>
-                        <button class="customer-button reset" type="reset">Reset</button>
-                        <br>
-                        <button class="customer-button apply" type="submit" value="submit">Apply</button>
-                      </form>
-                    </div>
-                  </div>
-                ';
                 // Output data of each row
                   while($row = $result->fetch_assoc()) {
                     // Create divs based on the data
                     echo '<div class="products-row" id="customer1" onclick="cartela(this.id)">';
-                    echo '<button class="cell-more-button menujs" onclick="moreoptions(' . $row['id'] .')"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg></button>';
-                    echo $customermenuphp;
+                    echo '<button class="cell-more-button " onclick="moreoptions(' . $row['id'] .')"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg></button>';
                     echo '<div class="product-cell image"><!--IMAGE GOES HERE--><span>' . $row['firstname'] .' '. $row['lastname'] . '</span></div>';
                     echo '<div class="product-cell category"><span class="cell-label">Company:</span>' . $row['company'] .'</div>';
                     echo '<div class="product-cell status-cell"><span class="cell-label">Address:</span><span>' . $row['addr'] .','. $row['zip'] . ','. $row['city'] .' '. $row['country'] .'</span></div>';
@@ -497,9 +465,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         });
         document.querySelector(".customerjs").addEventListener("click", function () {
             document.querySelector(".customer-menu").classList.toggle("active");
-        });
-        document.querySelector(".menujs").addEventListener("click", function () {
-            document.querySelector(".customermenu").classList.toggle("active");
         });
         document.querySelector(".grid").addEventListener("click", function () {
             document.querySelector(".list").classList.remove("active");
