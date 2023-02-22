048d30a70b0e154dded971e2834337d219f9d5eb
eskitzis <eskitzis@csd.auth.gr>
2023-02-22 23:31:34 +0200

    1131

--- customers.php
+++ customers.php
@@ -448,7 +448,7 @@ if(isset($_POST['order_submit'])){
                   while($row = $result->fetch_assoc()) {
                     // Create divs based on the data
                     echo '<div class="products-row" id="customer1" onclick="cartela(this.id)">';
-                    echo '<button id="menupopup" class="cell-more-button " onclick="myFunction(\''.$row["id"].'\', \''.$row["lastname"].'\'\')";><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg></button>';
+                    echo '<button id="menupopup" class="cell-more-button " onclick="moreoptions(\"".$row["id"]."\", \"".$row["lastname"]."\")";><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg></button>';
                     echo '<div class="product-cell image"><!--IMAGE GOES HERE--><span>' . $row['firstname'] .' '. $row['lastname'] . '</span></div>';
                     echo '<div class="product-cell category"><span class="cell-label">Company:</span>' . $row['company'] .'</div>';
                     echo '<div class="product-cell sales"><span class="cell-label">Address:</span><span>' . $row['addr'] .','. $row['zip'] . ','. $row['city'] .' '. $row['country'] .'</span></div>';
