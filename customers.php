cf2759654b522ef535579d806e222025beeddf0f
eskitzis <eskitzis@csd.auth.gr>
2023-02-24 01:37:13 +0200

    137

--- customers.php
+++ customers.php
@@ -330,7 +330,7 @@ if(isset($_POST['order_submit'])){
                       <!--DETAILED ORDER STATUS OF LAST ORDER / BIT CAN SELECT ORDER FROM DROPDOWN MENU-->
                       <div class="detailed-order">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
-                        <label for="">Customer Order</label>
+                        <label for="" class="total-label">Customer Order</label>
                         <span>-</span>
                       </div>
                       <div class="detailed-order">
