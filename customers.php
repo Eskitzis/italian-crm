fde1c331ad0daa24313f79a9db750bef9092a0bb
eskitzis <eskitzis@csd.auth.gr>
2023-02-24 01:00:06 +0200

    100

--- customers.php
+++ customers.php
@@ -566,8 +566,8 @@ if(isset($_POST['order_submit'])){
         const ctmodal = document.getElementById("customermodal");
         const ctspan = document.getElementsByClassName("ct")[0];
         function moreoptions(id,fname,lname) {
+          event.stopPropagation();
           modal.style.display = "block";
-          ctmodal.style.display = "none";
           document.getElementById('customerid').value = id;
           document.getElementById('customerlastname').value = fname +' '+ lname;
         }
