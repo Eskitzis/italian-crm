c6916b9893e7086900dfd6542ec26941aba3bd09
eskitzis <eskitzis@csd.auth.gr>
2023-02-24 01:23:35 +0200

    123

--- customers.php
+++ customers.php
@@ -331,36 +331,47 @@ if(isset($_POST['order_submit'])){
                       <div class="detailed-order">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
                         <label for="">Customer Order</label>
+                        <span>-</span>
                       </div>
                       <div class="detailed-order">
                         <label for="">GRUPPOCASA->FACTORY</label>
+                        <span>-</span>
                       </div>
                       <div class="detailed-order">
                         <label for="">FACTORY->CUSTOMER (PROFORMA)</label>
+                        <span>-</span>
                       </div>
                       <div class="detailed-order">
                         <label for="">GRUPPOCASA->CUSTOMER (PROFORMA)</label>
+                        <span>-</span>
                       </div>
                       <div class="detailed-order">
                         <label for="">CUSTOMER->GRUPPOCASA (CONFIRMATION)</label>
+                        <span>-</span>
                       </div>
                       <div class="detailed-order">
                         <label for="">GRUPPOCASA->FACTORY (CONFIRMATION)</label>
+                        <span>-</span>
                       </div>
                       <div class="detailed-order">
                         <label for="">Advance payment 1</label>
+                        <span>-</span>
                       </div>
                       <div class="detailed-order">
                         <label for="">Advance payment 2</label>
+                        <span>-</span>
                       </div>
                       <div class="detailed-order">
                         <label for="">Invoices</label>
+                        <span>-</span>
                       </div>
                       <div class="detailed-order">
                         <label for="">Final payment</label>
+                        <span>-</span>
                       </div>
                       <div class="detailed-order">
                         <label for="">Order Shipped</label>
+                        <span>-</span>
                       </div>  
                       <!--END DETAILED ORDER-->
                       </div>
