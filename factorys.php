b8dc8264c4e0a9e5b7e34f1c09117cd6b77f1ae7
eskitzis <eskitzis@csd.auth.gr>
2023-02-24 22:19:59 +0200

    1019

--- factorys.php
+++ factorys.php
@@ -11,6 +11,7 @@ $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
   die('Connection failed: ' . $conn->connect_error);
 }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
