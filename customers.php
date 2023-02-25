88eb8d634d1bfa18ff1249ed0ba8b20e5f1c0bc9
eskitzis <eskitzis@csd.auth.gr>
2023-02-25 23:24:06 +0200

    1124

--- customers.php
+++ customers.php
@@ -336,11 +336,13 @@ if(isset($_POST['order_submit'])){
                           }
                         ?>
                         <input type="hidden" name="customerid" id="customerid">
-                        <select name="orders" id="" class="select-cartela">
-                        <?php foreach ($orders as $order) { ?>
-                          <option value="<?php echo $order['id']; ?>"><?php echo $order['first_update']; ?></option>
-                        <?php } ?>
-                        </select>
+                        <div class="div-select">
+                          <select name="orders" id="" class="select-cartela">
+                          <?php foreach ($orders as $order) { ?>
+                            <option value="<?php echo $order['id']; ?>"><?php echo $order['first_update']; ?></option>
+                          <?php } ?>
+                          </select>
+                        </div>
                       <!--DETAILED ORDER STATUS OF LAST ORDER / BIT CAN SELECT ORDER FROM DROPDOWN MENU-->
                       <div class="detailed-order">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
