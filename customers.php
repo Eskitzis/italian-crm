3734d9254034bc513703df85addc79b6b94c8843
eskitzis <eskitzis@csd.auth.gr>
2023-02-27 15:07:18 +0200

    307

--- customers.php
+++ customers.php
@@ -57,7 +57,6 @@ if(isset($_POST['order_submit'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script>const ws = new WebSocket("wss://" + location.host + "/")</script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
