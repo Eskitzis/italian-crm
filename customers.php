56b34a9c07a462151a9a8a68da612ac14f5c2802
eskitzis <eskitzis@csd.auth.gr>
2023-02-28 13:16:48 +0200

    116

--- customers.php
+++ customers.php
@@ -243,7 +243,7 @@ if(isset($_POST['order_submit'])){
                 // Output data of each row
                   while($row = $result->fetch_assoc()) {
                     // Create divs based on the data
-                    echo '<div class="products-row" id="queryBtn" onclick="cartela(\''.$row['id'].'\', \''.$row['firstname'].'\', \''.$row['lastname'].'\');deleteCookies()">';
+                    echo '<div class="products-row" id="queryBtn" onclick="cartela(\''.$row['id'].'\', \''.$row['firstname'].'\', \''.$row['lastname'].'\')">';
                     echo '<button id="menupopup" class="cell-more-button" onclick="moreoptions(\''.$row['id'].'\', \''.$row['firstname'].'\', \''.$row['lastname'].'\')";><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg></button>';
                     echo '<div class="product-cell image"><!--IMAGE GOES HERE--><span>' . $row['firstname'] .' '. $row['lastname'] . '</span></div>';
                     echo '<div class="product-cell category"><span class="cell-label">Company:</span>' . $row['company'] .'</div>';
@@ -532,18 +532,6 @@ if(isset($_POST['order_submit'])){
       </div>
 </div>
     <script>
-      function deleteCookies() {
-        var allCookies = document.cookie.split(';');
-        for (var i = 0; i < allCookies.length; i++)
-          document.cookie = allCookies[i] + "=;expires="
-          + new Date(0).toUTCString();
-          displayCookies.innerHTML = document.cookie;
-      }
-      document.querySelectorAll('a').forEach(link => {
-        link.addEventListener('click', event => {
-          event.preventDefault();
-        });
-      });
         if ( window.history.replaceState ) {
             window.history.replaceState( null, null, window.location.href );
         }
@@ -600,6 +588,7 @@ if(isset($_POST['order_submit'])){
           document.getElementById('customerlastname').value = fname +' '+ lname;
         }
         function cartela(id,fname,lname) {
+          document.cookie = 'id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
           ctmodal.style.display = "block";
           document.getElementById('cartelacustomerid').value = id;
           document.getElementById('customername').innerHTML = fname +' '+ lname;
