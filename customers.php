c6dab60bc58ebd1a4bbf4e3d231405ecba31cd00
eskitzis <eskitzis@csd.auth.gr>
2023-02-28 09:52:13 +0200

    952

--- customers.php
+++ customers.php
@@ -310,7 +310,7 @@ if(isset($_POST['order_submit'])){
           <span class="close ct">&times;</span>
           <h2 id="customername">Customer</h2>
           <div style="text-align: right;">
-            <input class="customerid deactivated" type="text" data-noreset="true" name="cartelacustomerid" id="cartelacustomerid" readonly>
+            <input class="customerid deactivated" style="text-align: center;" type="text" data-noreset="true" name="cartelacustomerid" id="cartelacustomerid" readonly>
           </div>
           <div class="customer-container" id="customercontainer">
                     <div id="btn-group" class="column full">
