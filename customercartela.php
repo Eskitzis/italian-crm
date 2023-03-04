51d26d6e6b2bf7beb2097e6ca9f6275fcb7b2ff5
eskitzis <eskitzis@csd.auth.gr>
2023-03-05 00:52:46 +0200

    1252

--- customercartela.php
+++ customercartela.php
@@ -77,14 +77,14 @@
           <!--DETAILED ORDER STATUS OF LAST ORDER / BIT CAN SELECT ORDER FROM DROPDOWN MENU-->
           <div class="detailed-order">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
-            <div style="text-align:left;" >
+            <div style="float:left;" >
                 <label for="" class="label-customer">Customer Order</label>
             </div>
             <?php
               $sql = "SELECT * FROM order_status WHERE order_status = 'Customer Order' AND customer_id = '$id'";
               $result = mysqli_query($conn, $sql);
               $row = mysqli_fetch_assoc($result);
-              echo '<div style="text-align:right;">'.$row['order_update'].'</div>';
+              echo '<div style="float:right;"><span class="span-customer">'.$row['order_update'].'</span></div>';
             ?>
           </div>
           <div class="detailed-order">
