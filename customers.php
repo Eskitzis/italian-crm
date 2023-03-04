564a77b0904a503618c443cfd5bfe32e9fb49332
eskitzis <eskitzis@csd.auth.gr>
2023-03-04 23:28:43 +0200

    1128

--- customers.php
+++ customers.php
@@ -355,14 +355,14 @@ if(isset($_POST['order_submit'])){
           </div>
           <!--DETAILED ORDER STATUS OF LAST ORDER / BIT CAN SELECT ORDER FROM DROPDOWN MENU-->
           <div class="detailed-order">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
-            <div style="float:left;">
-              <label for="" class="label-customer">Customer Order</label>
-            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>            
             <?php
               $sql = "SELECT * FROM order_status WHERE order_status = 'Customer Order' AND customer_id = '1'";
               $result = mysqli_query($conn, $sql);
               $row = mysqli_fetch_assoc($result);
+              echo '<div style="float:left;">';
+              echo  $row['order_status'];
+              echo '</div>';
               echo '<div style="float:right;">';
               echo  $row['order_update'];
               echo '</div>';
