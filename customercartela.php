90405588eb52d4c13d97dd63cff5a748aa47965b
eskitzis <eskitzis@csd.auth.gr>
2023-03-05 16:54:59 +0200

    454

--- customercartela.php
+++ customercartela.php
@@ -80,7 +80,7 @@
           <div class="detailed-order">
             <div>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
-              <h6>Customer Order</h6>
+              <label for="" class="label-customer">Customer Order</label>
               <?php
                 $sql = "SELECT * FROM order_status WHERE order_status = 'Customer Order' AND customer_id = '$id'";
                 $result = mysqli_query($conn, $sql);
