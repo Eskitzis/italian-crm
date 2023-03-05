6a8d40e79e512d65600e8c6c89e57be998e22b10
eskitzis <eskitzis@csd.auth.gr>
2023-03-05 18:19:55 +0200

    619

--- orders.php
+++ orders.php
@@ -168,7 +168,7 @@ if ($conn->connect_error) {
                   while($row = $result->fetch_assoc()) {
                     // Create divs based on the data
                     echo '<div class="products-row">';
-                    echo '<button class="cell-more-button" onclick="moreoptions(\''.$row['id'].'\', \''.$row['firstname'].'\', \''.$row['lastname'].'\')";><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg></button>';
+                    echo '<button class="cell-more-button" onclick="moreoptions(\''.$row['id'].'\', \''.$row['customer_id'].'\', \''.$row['representative'].'\')";><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg></button>';
                     echo '<div class="product-cell image"><!--IMAGE GOES HERE--><span>' . $row['name'] .'</span></div>';
                     echo '<div class="product-cell category"><span class="cell-label">Category:</span>' . $row['category'] .'</div>';
                     echo '<div class="product-cell status-cell"><span class="cell-label">Status:</span><span class="status active">' . $row['status'] .'</span></div>';
