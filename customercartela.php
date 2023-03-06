5894a56181704789b68403e2d61aaeb6b3abc740
eskitzis <eskitzis@csd.auth.gr>
2023-03-06 14:09:30 +0200

    209

--- customercartela.php
+++ customercartela.php
@@ -150,7 +150,7 @@
                     }
                   ?>
                   <div>
-                    <select name="orders">
+                    <select class="orders-select" name="orders">
                       <?php foreach ($orders as $order) { ?>
                       <option value="<?php echo $order['id']; ?>"><?php echo $order['first_update']; ?></option>
                       <?php } ?>
