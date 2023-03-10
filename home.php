042130866902a8e20df4121146112ac0636a326b
eskitzis <eskitzis@csd.auth.gr>
2023-03-10 17:12:43 +0200

    512

--- home.php
+++ home.php
@@ -220,62 +220,62 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             </article>
 
                             <article class="leaderboard__profile" onclick="ord2()">
-                                <img src="assets/detailed_order/offering-customer.jpg" alt="GRUPPOCASA->FACTORY" class="leaderboard__icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-upload"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="17 8 12 3 7 8"></polyline><line x1="12" y1="3" x2="12" y2="15"></line></svg>
                                 <span class="leaderboard__name">GRUPPOCASA->FACTORY</span>
                                 <span class="leaderboard__value">21.01.2023<span style="color: black;">B<span style="color: red;">12</span></span></span>
                             </article>
         
                             <article class="leaderboard__profile" onclick="ord3()">
-                                <img src="assets/detailed_order/cost.png" alt="FACTORY->CUSTOMER (PROFORMA)" class="leaderboard__icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
                                 <span class="leaderboard__name">FACTORY->CUSTOMER (PROFORMA)</span>
                                 <span class="leaderboard__value">22.01.2023<span style="color: black;">C<span style="color: red;">31</span></span></span>
                             </article>
         
                             <article class="leaderboard__profile" onclick="ord4()">
-                                <img src="assets/detailed_order/factory.png" alt="GRUPPOCASA->CUSTOMER (PROFORMA)" class="leaderboard__icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
                                 <span class="leaderboard__name">GRUPPOCASA->CUSTOMER (PROFORMA)</span>
                                 <span class="leaderboard__value">23.01.2023<span style="color: black;">D<span style="color: red;">24</span></span></span>
                             </article>  
 
                             <article class="leaderboard__profile" onclick="ord5()">
-                                <img src="assets/detailed_order/manufacturing.png" alt="CUSTOMER->GRUPPOCASA (CONFIRMATION)" class="leaderboard__icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
                                 <span class="leaderboard__name">CUSTOMER->GRUPPOCASA (CONFIRMATION)</span>
                                 <span class="leaderboard__value">24.01.2023<span style="color: black;">E<span style="color: red;">34</span></span></span>
                             </article>
         
                             <article class="leaderboard__profile" onclick="ord6()">
-                                <img src="assets/detailed_order/order-delivery.png" alt="GRUPPOCASA->FACTORY (CONFIRMATION)" class="leaderboard__icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
                                 <span class="leaderboard__name">GRUPPOCASA->FACTORY (CONFIRMATION)</span>
                                 <span class="leaderboard__value">25.01.2023<span style="color: black;">F<span style="color: red;">0</span></span></span>
                             </article>      
                         </div>
                         <div class="flex-item-right">
                             <article class="leaderboard__profile" onclick="ord7()">
-                                <img src="assets/detailed_order/fast.png" alt="Advance payment 1" class="leaderboard__icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-credit-card"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></svg>
                                 <span class="leaderboard__name">Advance payment 1</span>
                                 <span class="leaderboard__value">26.01.2023<span style="color: black;">G<span style="color: red;">23</span></span></span>
                             </article>
         
                             <article class="leaderboard__profile" onclick="ord8()">
-                                <img src="assets/detailed_order/order.png" alt="Advance payment 2" class="leaderboard__icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-credit-card"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></svg>
                                 <span class="leaderboard__name">Advance payment 2</span>
                                 <span class="leaderboard__value">27.01.2023<span style="color: black;">H<span style="color: red;">12</span></span></span>
                             </article>
 
                             <article class="leaderboard__profile" onclick="ord8()">
-                                <img src="assets/detailed_order/order.png" alt="Invoices" class="leaderboard__icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
                                 <span class="leaderboard__name">Invoices</span>
                                 <span class="leaderboard__value">27.01.2023<span style="color: black;">H<span style="color: red;">12</span></span></span>
                             </article>
 
                             <article class="leaderboard__profile" onclick="ord8()">
-                                <img src="assets/detailed_order/order.png" alt="Final payment" class="leaderboard__icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-credit-card"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></svg>
                                 <span class="leaderboard__name">Final payment</span>
                                 <span class="leaderboard__value">27.01.2023<span style="color: black;">H<span style="color: red;">12</span></span></span>
                             </article>
 
                             <article class="leaderboard__profile" onclick="ord8()">
-                                <img src="assets/detailed_order/order.png" alt="Order Shipped" class="leaderboard__icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="margin-left: 15px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-package"><line x1="16.5" y1="9.4" x2="7.5" y2="4.21"></line><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg>
                                 <span class="leaderboard__name">Order Shipped</span>
                                 <span class="leaderboard__value">27.01.2023<span style="color: black;">H<span style="color: red;">12</span></span></span>
                             </article>
