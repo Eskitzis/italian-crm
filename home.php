da1c6bc3fddc548ab6c339fb4201ce30ce8b7711
eskitzis <eskitzis@csd.auth.gr>
2023-03-13 15:16:32 +0200

    416

--- home.php
+++ home.php
@@ -993,8 +993,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </div>
             </div>
             <div class="flex-item-three">
-              <canvas id="orderstatuschart" class="canvas-css"></canvas>
-              <canvas id="ordersfulfilled" class="canvas-css"></canvas>                                      
+              <canvas id="orderstatuschart" style="width:50%;max-width:100%"></canvas>
+              <canvas id="ordersfulfilled" style="width:50%;max-width:100%"></canvas>                                      
             </div>
         </div>
       </div>
